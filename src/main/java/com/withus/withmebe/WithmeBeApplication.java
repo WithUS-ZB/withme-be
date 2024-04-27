2b4f6ef44acb9190e997d7b1ce34ca8daec99a9a
gamgyul163 <gamgyul163@gmail.com>
2024-04-27 16:18:45 +0900

    begin : 프로젝트 생성

---
+++ src/main/java/com/withus/withmebe/WithmeBeApplication.java
@@ -0,0 +1,13 @@
+package com.withus.withmebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithmeBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WithmeBeApplication.class, args);
+  }
+
+}
