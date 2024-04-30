2f0134ab1dd0d455e2cbebbbc03ff1a915bf315c
je-pa <jieun0502@gmail.com>
2024-05-01 00:43:17 +0900

    ✨ Features: member 간략 조회

---
+++ src/main/java/com/withus/withmebe/member/controller/MemberController.java
@@ -0,0 +1,23 @@
+package com.withus.withmebe.member.controller;
+
+import com.withus.withmebe.member.dto.MemberSimpleDetailDto;
+import com.withus.withmebe.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/member")
+@RequiredArgsConstructor
+public class MemberController {
+  private final MemberService memberService;
+
+  @GetMapping("/detail/{userId}")
+  public ResponseEntity<MemberSimpleDetailDto> get(@PathVariable Long userId){
+    return ResponseEntity.ok(memberService.read(userId));
+  }
+
+}
