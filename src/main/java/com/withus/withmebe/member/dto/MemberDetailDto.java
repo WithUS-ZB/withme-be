371e71c9fa1623475ffe3a4e1471ac4d7b5f0679
je-pa <jieun0502@gmail.com>
2024-05-01 01:41:15 +0900

    ✨ Features: member 내 정보 상세 조회

---
+++ src/main/java/com/withus/withmebe/member/dto/MemberDetailDto.java
@@ -0,0 +1,38 @@
+package com.withus.withmebe.member.dto;
+
+import com.withus.withmebe.member.entity.Member;
+import com.withus.withmebe.member.type.Gender;
+import com.withus.withmebe.member.type.Membership;
+import com.withus.withmebe.member.type.SignupPath;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public record MemberDetailDto(
+    Long memberId,
+    String email,
+    String nickName,
+    LocalDate birthDate,
+    Gender gender,
+    String phoneNumber,
+    String profileImg,
+    SignupPath signupPath,
+    LocalDateTime signupDttm,
+    Membership membership
+
+) {
+
+  public static MemberDetailDto fromEntity(Member member) {
+    return new MemberDetailDto(
+        member.getId(),
+        member.getEmail(),
+        member.getNickName(),
+        member.getBirthDate(),
+        member.getGender(),
+        member.getPhoneNumber(),
+        member.getProfileImg(),
+        member.getSignupPath(),
+        member.getSignupDttm(),
+        member.getMembership()
+    );
+  }
+}
--- src/main/java/com/withus/withmebe/member/service/MemberService.java
+++ src/main/java/com/withus/withmebe/member/service/MemberService.java
@@ -3,8 +3,11 @@ package com.withus.withmebe.member.service;
 import static com.withus.withmebe.common.exception.ExceptionCode.ENTITY_NOT_FOUND;
 
 import com.withus.withmebe.common.exception.CustomException;
+import com.withus.withmebe.member.dto.MemberDetailDto;
 import com.withus.withmebe.member.dto.MemberSimpleDetailDto;
+import com.withus.withmebe.member.entity.Member;
 import com.withus.withmebe.member.repository.MemberRepository;
+import com.withus.withmebe.security.util.MySecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +16,20 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
   private final MemberRepository memberRepository;
-  public MemberSimpleDetailDto read(Long userId) {
+  public MemberSimpleDetailDto read(Long memberId) {
     return MemberSimpleDetailDto.fromEntity(
-      memberRepository.findById(userId)
-          .orElseThrow(() -> new CustomException(ENTITY_NOT_FOUND))
+        getMemberById(memberId)
     );
+  }
+
+  public MemberDetailDto readCurretLoginMemberDetail() {
+    return MemberDetailDto.fromEntity(
+        getMemberById(MySecurityUtil.getCurrentLoginMemberId())
+    );
+  }
+
+  private Member getMemberById(Long id) {
+    return memberRepository.findById(id)
+        .orElseThrow(() -> new CustomException(ENTITY_NOT_FOUND));
   }
 }
