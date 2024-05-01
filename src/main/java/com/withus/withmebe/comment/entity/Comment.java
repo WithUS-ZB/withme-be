2e621d83a39e32b73165c774a23c65cdc8a5cbab
gamgyul163 <gamgyul163@gmail.com>
2024-05-01 19:07:49 +0900

    ♻️ Refactor: @EntityListener 어노테이션 베이스 엔티티와 중복으로 들어가 있던것 삭제

--- src/main/java/com/withus/withmebe/comment/entity/Comment.java
+++ src/main/java/com/withus/withmebe/comment/entity/Comment.java
@@ -15,14 +15,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Where(clause = "deletedDttm is null")
 @SQLDelete(sql = "UPDATE comment SET deletedDttm = LocalDateTime.now() WHERE id = ?")
-@EntityListeners(value = AuditingEntityListener.class)
 public class Comment extends BaseEntity {
 
   @Id
