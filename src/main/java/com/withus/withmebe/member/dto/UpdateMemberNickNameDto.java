3f1e4ba46d318d569f07d593f072f3a7150ef0db
je-pa <jieun0502@gmail.com>
2024-05-03 02:11:29 +0900

    ✨ Features: member 프로필이미지/닉네임 업데이트

---
+++ src/main/java/com/withus/withmebe/member/dto/UpdateMemberNickNameDto.java
@@ -0,0 +1,24 @@
+package com.withus.withmebe.member.dto;
+
+import com.withus.withmebe.member.entity.Member;
+import jakarta.validation.constraints.NotBlank;
+
+public record UpdateMemberNickNameDto() {
+
+  public record Request(
+      @NotBlank
+      String nickName
+  ) {
+
+  }
+
+  public record Response(
+      String nickName
+  ) {
+
+    public static Response fromEntity(Member currentMember) {
+      return new Response(
+          currentMember.getNickName());
+    }
+  }
+}
