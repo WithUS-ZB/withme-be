881756d4cb9a73f92ba5eeaf9821e86a7a90f3f0
Stone <kangrack333@gmail.com>
2024-05-03 12:33:23 +0900

    feat : Gathering Repository 구현

---
+++ src/main/java/com/withus/withmebe/gathering/repository/GatheringRepository.java
@@ -0,0 +1,8 @@
+package com.withus.withmebe.gathering.repository;
+
+import com.withus.withmebe.gathering.entity.Gathering;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GatheringRepository extends JpaRepository<Gathering, Long> {
+
+}
