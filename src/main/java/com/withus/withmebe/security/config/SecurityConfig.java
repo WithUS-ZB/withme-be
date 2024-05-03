ea049fedf3d4b310a9d17b77e182cd4d38f3333c
je-pa <jieun0502@gmail.com>
2024-05-03 16:59:52 +0900

    ♻️ Refactor code: security 설정 변경

--- src/main/java/com/withus/withmebe/security/config/SecurityConfig.java
+++ src/main/java/com/withus/withmebe/security/config/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
   }
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http,
+      CorsConfigurationSource corsConfigurationSource) throws Exception {
     http
         .httpBasic(AbstractHttpConfigurer::disable)
         .csrf(AbstractHttpConfigurer::disable)
@@ -40,7 +41,10 @@ public class SecurityConfig {
         .sessionManagement(sessionManagement ->
             sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorizeRequests ->
-            authorizeRequests.requestMatchers("/api/auth/signup", "/api/auth/signin", "/api/comment/list/*").permitAll()
+            authorizeRequests.requestMatchers("/api/auth/signup"
+                    , "/api/auth/signin"
+                    , "/api/comment/list/*"
+                    , "/api/check/email").permitAll()
                 .anyRequest().authenticated())
         .addFilterBefore(this.authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
@@ -53,7 +57,7 @@ public class SecurityConfig {
 
     config.setAllowCredentials(true);
     config.setAllowedOriginPatterns(Arrays.asList("*"));
-    config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
+    config.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT"));
     config.setAllowedHeaders(Arrays.asList("*"));
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
