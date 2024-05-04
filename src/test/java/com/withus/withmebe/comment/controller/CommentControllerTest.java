5723d1cee04f7e2132136d3588cbf200abfc2080
gamgyul163 <gamgyul163@gmail.com>
2024-05-04 23:58:18 +0900

    ✅ Test: getComments 테스트 추가

--- src/test/java/com/withus/withmebe/comment/controller/CommentControllerTest.java
+++ src/test/java/com/withus/withmebe/comment/controller/CommentControllerTest.java
@@ -3,6 +3,7 @@ package com.withus.withmebe.comment.controller;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,6 +16,7 @@ import com.withus.withmebe.common.exception.CustomException;
 import com.withus.withmebe.common.exception.ExceptionCode;
 import com.withus.withmebe.security.jwt.filter.JwtAuthenticationFilter;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +24,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import util.WithMockCustomUser;
@@ -41,6 +47,7 @@ class CommentControllerTest {
   private MockMvc mockMvc;
 
   private final String baseUrl = "/api/comment";
+  private final long gatheringId = 1L;
   private final Gson gson = new Gson();
   private static final JsonObject jsonObject = new JsonObject();
 
@@ -113,7 +120,6 @@ class CommentControllerTest {
     //given
     given(commentService.createComment(anyLong(), anyLong(), any()))
         .willThrow(new CustomException(ExceptionCode.AUTHENTICATION_ISSUE));
-
     //when
     //then
     mockMvc.perform(post(baseUrl + "/add?gatheringid=1")
@@ -123,4 +129,53 @@ class CommentControllerTest {
         .andExpect(status().isUnauthorized());
   }
 
+  @Test
+  @WithMockUser
+  void seccessToGetComments() throws Exception {
+    //given
+    CommentResponse commentResponse2 = CommentResponse.builder()
+        .id(2L)
+        .nickName("홍길동2")
+        .commentContent("내용2")
+        .createdDttm(LocalDateTime.of(2023, 4, 5, 6, 7, 8))
+        .updatedDttm(LocalDateTime.of(2024, 5, 6, 7, 8, 9))
+        .build();
+    Pageable pageable = PageRequest.of(0, 10);
+
+    given(commentService.readComments(anyLong(), any()))
+        .willReturn(
+            new PageImpl<CommentResponse>(List.of(commentResponse, commentResponse2), pageable, 2));
+
+    //when
+    //then
+    mockMvc.perform(get(baseUrl + "/list/" + gatheringId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .with(SecurityMockMvcRequestPostProcessors.csrf()))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.totalElements").value(2))
+        .andExpect(jsonPath("$.totalPages").value(1))
+        .andExpect(jsonPath("$.pageable.pageNumber").value(0))
+        .andExpect(jsonPath("$.content[0].id").value(commentResponse.id()))
+        .andExpect(jsonPath("$.content[0].nickName").value(commentResponse.nickName()))
+        .andExpect(jsonPath("$.content[0].commentContent").value(commentResponse.commentContent()))
+        .andExpect(jsonPath("$.content[0].createdDttm").value(commentResponse.createdDttm().toString()))
+        .andExpect(jsonPath("$.content[0].updatedDttm").value(commentResponse.updatedDttm().toString()))
+        .andExpect(jsonPath("$.content[1].id").value(commentResponse2.id()))
+        .andExpect(jsonPath("$.content[1].nickName").value(commentResponse2.nickName()))
+        .andExpect(jsonPath("$.content[1].commentContent").value(commentResponse2.commentContent()))
+        .andExpect(jsonPath("$.content[1].createdDttm").value(commentResponse2.createdDttm().toString()))
+        .andExpect(jsonPath("$.content[1].updatedDttm").value(commentResponse2.updatedDttm().toString()));
+  }
+
+  @Test
+  @WithMockUser
+  void failToGetCommentsByBadRequest() throws Exception {
+    //given
+    //when
+    //then
+    mockMvc.perform(get(baseUrl + "/list")
+            .contentType(MediaType.APPLICATION_JSON)
+            .with(SecurityMockMvcRequestPostProcessors.csrf()))
+        .andExpect(status().isBadRequest());
+  }
 }
