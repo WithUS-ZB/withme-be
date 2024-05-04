bc03e838bca1cb39043408f5bc43539383eaa799
gamgyul163 <gamgyul163@gmail.com>
2024-05-04 13:06:19 +0900

    ♻️ Refactor: fetch join sql 어노테이션 사용하지 않고 entitygraph로 적용

--- src/main/java/com/withus/withmebe/comment/repository/CommentRepository.java
+++ src/main/java/com/withus/withmebe/comment/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package com.withus.withmebe.comment.repository;
 import com.withus.withmebe.comment.entity.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-  @Query(value = "select c from Comment c join fetch c.member where c.gatheringId = :gatheringId", countQuery = "select count(c) from Comment c where c.gatheringId = :gatheringId")
+  @EntityGraph(attributePaths = "member")
   Page<Comment> findCommentsByGatheringId(long gatheringId, Pageable pageable);
 
 }
