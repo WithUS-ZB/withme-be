4886f96f8a29e01978c417ce3cbd47b230cd8a72
gamgyul163 <gamgyul163@gmail.com>
2024-05-06 16:47:35 +0900

    ♻️ Refactor: GatheringSearch에서 불필요한 필드 제거

--- src/main/java/com/withus/withmebe/search/document/GatheringSearch.java
+++ src/main/java/com/withus/withmebe/search/document/GatheringSearch.java
@@ -1,9 +1,7 @@
 package com.withus.withmebe.search.document;
 
-import com.withus.withmebe.gathering.Type.GatheringType;
 import com.withus.withmebe.gathering.Type.ParticipantSelectionMethod;
 import com.withus.withmebe.gathering.Type.ParticipantsType;
-import com.withus.withmebe.gathering.Type.Status;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import org.springframework.data.annotation.Id;
@@ -18,54 +16,31 @@ import org.springframework.data.elasticsearch.annotations.Setting;
 public record GatheringSearch(
 
     @Id
-    @Field(name = "gathering_id", type = FieldType.Long)
+    @Field(name = "gathering_id")
     Long id,
 
-    @Field(type = FieldType.Text)
-    String memberNickName,
-    @Field(type = FieldType.Text)
+    String nickName,
     String title,
-    @Field(type = FieldType.Text)
-    String content,
 
-    @Field(type = FieldType.Keyword)
-    GatheringType gatheringType,
-    @Field(type = FieldType.Long)
     Long maximumParticipant,
-
-    @Field(type = FieldType.Date)
     LocalDateTime startDttm,
-    @Field(type = FieldType.Date)
     LocalDateTime endDttm,
-    @Field(type = FieldType.Keyword)
-    String category,
-    @Field(type = FieldType.Date)
     LocalDateTime applicationDeadLine,
 
-    @Field(type = FieldType.Text)
     String address,
-    @Field(type = FieldType.Text)
     String detailedAddress,
-    @Field(type = FieldType.Object)
-    String location,
-
     String mainImg,
 
     @Field(type = FieldType.Keyword)
     ParticipantsType participantsType,
     Long fee,
-
-    @Field(type = FieldType.Keyword)
     ParticipantSelectionMethod participantSelectionMethod,
     Long likeCount,
-
-    @Field(type = FieldType.Keyword)
-    Status status,
 
     @Field(type = FieldType.Date)
     LocalDateTime createdDttm,
     @Field(type = FieldType.Date)
-    LocalDateTime updatedDttm
+    LocalDateTime deletedDttm
 ) {
 
 }
