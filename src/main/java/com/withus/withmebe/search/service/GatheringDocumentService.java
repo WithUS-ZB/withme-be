3bd5a806dfe1e88aab9867a3edeb8f06b723188f
gamgyul163 <gamgyul163@gmail.com>
2024-05-07 15:41:19 +0900

    ♻️ Refactor: 클래스 네이밍 변경

---
+++ src/main/java/com/withus/withmebe/search/service/GatheringDocumentService.java
@@ -0,0 +1,25 @@
+package com.withus.withmebe.search.service;
+
+import com.withus.withmebe.gathering.Type.Status;
+import com.withus.withmebe.search.document.GatheringDocument;
+import com.withus.withmebe.search.repository.GatheringDocumentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class GatheringDocumentService {
+
+  private final GatheringDocumentRepository gatheringDocumentRepository;
+
+  public Page<GatheringDocument> readGatheringSearches(String query,
+      String status, Pageable pageable) {
+    return gatheringDocumentRepository.searchByTitleAndStatusEqualsAndDeletedDttmIsNull(query,
+        status, pageable);
+  }
+}
