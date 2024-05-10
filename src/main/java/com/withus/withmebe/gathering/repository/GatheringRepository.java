145d4e4faebc5fa007c4e0e52898e43121ca609e
Stone <kangrack333@gmail.com>
2024-05-10 20:03:12 +0900

    ♻️ Refactor: 모임 레퍼지토리 리팩토링  - 코드 공백수정

--- src/main/java/com/withus/withmebe/gathering/repository/GatheringRepository.java
+++ src/main/java/com/withus/withmebe/gathering/repository/GatheringRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GatheringRepository extends JpaRepository<Gathering, Long> {
-
     List<Gathering> findAll();
     Optional<Gathering> findById(Long aLong);
 }
