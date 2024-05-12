3f273598470ce07753a67010348cbd327e5745ed
gamgyul163 <gamgyul163@gmail.com>
2024-05-12 20:29:07 +0900

    ✨ Features:현재 참여 인원 수 조회 추가(비관적 락)

--- src/main/java/com/withus/withmebe/participation/repository/ParticipationRepository.java
+++ src/main/java/com/withus/withmebe/participation/repository/ParticipationRepository.java
@@ -3,10 +3,12 @@ package com.withus.withmebe.participation.repository;
 import com.withus.withmebe.gathering.entity.Gathering;
 import com.withus.withmebe.participation.entity.Participation;
 import com.withus.withmebe.participation.type.Status;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -15,6 +17,9 @@ public interface ParticipationRepository extends JpaRepository<Participation, Lo
   boolean existsByParticipant_IdAndGathering_IdAndStatusIsNot(Long requesterId,Long gatheringId, Status status);
 
   long countByGathering_IdAndStatus(Long gatheringId, Status status);
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  long countByGatheringAndStatus(Gathering gathering, Status status);
 
   @EntityGraph(attributePaths = "participant")
   Page<Participation> findByGathering_Id(Long gatheringId, Pageable pageable);
