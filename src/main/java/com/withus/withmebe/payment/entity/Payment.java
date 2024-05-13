967147fa71f08c18004990251171401132f86d93
gamgyul163 <gamgyul163@gmail.com>
2024-05-13 21:23:25 +0900

    ♻️ Refactor: payment에 결제 방법 및 BaseEntity 상속 추가

--- src/main/java/com/withus/withmebe/payment/entity/Payment.java
+++ src/main/java/com/withus/withmebe/payment/entity/Payment.java
@@ -1,16 +1,17 @@
 package com.withus.withmebe.payment.entity;
 
-import com.withus.withmebe.member.entity.Member;
+import com.withus.withmebe.common.entity.BaseEntity;
+import com.withus.withmebe.payment.dto.request.ApprovePaymentRequest;
 import com.withus.withmebe.payment.dto.response.AddPaymentResponse;
+import com.withus.withmebe.payment.dto.response.ApprovePaymentResponse;
+import com.withus.withmebe.payment.type.PayMethod;
 import com.withus.withmebe.payment.type.Status;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import java.util.Objects;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,16 +19,15 @@ import lombok.NoArgsConstructor;
 @Entity
 @Getter
 @NoArgsConstructor
-public class Payment {
+public class Payment extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "payment_id")
   private Long id;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(nullable = false, name = "member_id")
-  private Member payer;
+  @Column(nullable = false)
+  private Long memberId;
 
   @Column(nullable = false)
   private String goodName;
@@ -35,25 +35,50 @@ public class Payment {
   @Column(nullable = false)
   private Long goodPrice;
 
-  private String traceNo;
+  private String tradeNo;
 
   @Column(nullable = false)
   private Status status = Status.CREATED;
 
+  private PayMethod payMethod;
+
   @Builder
-  public Payment(Member payer, String goodName, long goodPrice) {
-    this.payer = payer;
+  public Payment(long payerId, String goodName, long goodPrice) {
+    this.memberId = payerId;
     this.goodName = goodName;
     this.goodPrice = goodPrice;
   }
 
-  public AddPaymentResponse toAddPaymentResponse(String siteCd) {
+  public AddPaymentResponse toAddPaymentResponse(String siteCd, String certInfo) {
     return AddPaymentResponse.builder()
         .siteCd(siteCd)
         .id(this.id)
         .goodName(this.goodName)
         .goodPrice(this.goodPrice)
-        .payerId(this.payer.getId())
+        .payerId(this.memberId)
+        .certInfo(certInfo)
+        .build();
+  }
+
+  public void approve(ApprovePaymentRequest request) {
+    this.tradeNo = request.tradeNo();
+    this.payMethod = request.payMethod();
+    this.status = Status.APPROVED;
+  }
+
+  public boolean isValidApproveRequest(ApprovePaymentRequest request) {
+    if (!Objects.equals(request.amount(), this.goodPrice)) {
+      return false;
+    }
+    return Objects.equals(this.memberId, request.payerId());
+  }
+
+  public ApprovePaymentResponse toApprovePaymentResponse() {
+    return ApprovePaymentResponse.builder()
+        .id(this.id)
+        .goodName(this.goodName)
+        .goodPrice(this.goodPrice)
+        .updatedDttm(this.getUpdatedDttm())
         .build();
   }
 }
