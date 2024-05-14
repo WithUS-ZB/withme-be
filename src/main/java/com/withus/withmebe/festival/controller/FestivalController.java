02232d07fa54e66a5d2bdaace92065140024e0fb
gamgyul163 <gamgyul163@gmail.com>
2024-05-14 23:23:40 +0900

    ✨ Features: 축제 정보 조회에 pageable 추가, 특정 시간마다 정보 불러오기 추가

--- src/main/java/com/withus/withmebe/festival/controller/FestivalController.java
+++ src/main/java/com/withus/withmebe/festival/controller/FestivalController.java
@@ -2,8 +2,10 @@ package com.withus.withmebe.festival.controller;
 
 import com.withus.withmebe.festival.dto.FestivalSimpleInfo;
 import com.withus.withmebe.festival.service.FestivalService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,9 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/festival")
 @RequiredArgsConstructor
 public class FestivalController {
+
   private final FestivalService festivalService;
+
   @GetMapping
-  public ResponseEntity<List<FestivalSimpleInfo>> getFestivals() {
-    return ResponseEntity.ok(festivalService.readFestivals());
+  public ResponseEntity<Page<FestivalSimpleInfo>> getFestivals(
+      @PageableDefault(size = 6) Pageable pageable) {
+    return ResponseEntity.ok(festivalService.readFestivals(pageable));
   }
 }
--- src/main/java/com/withus/withmebe/festival/repository/FestivalRepository.java
+++ src/main/java/com/withus/withmebe/festival/repository/FestivalRepository.java
@@ -2,10 +2,11 @@ package com.withus.withmebe.festival.repository;
 
 import com.withus.withmebe.festival.entity.Festival;
 import java.time.LocalDateTime;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FestivalRepository extends JpaRepository<Festival, Long> {
 
-  List<Festival> findFestivalByStartDttmBeforeAndEndDttmAfter(LocalDateTime currentTime1, LocalDateTime currentTime2);
+  Page<Festival> findFestivalByStartDttmBeforeAndEndDttmAfter(LocalDateTime currentTime1, LocalDateTime currentTime2, Pageable pageable);
 }
