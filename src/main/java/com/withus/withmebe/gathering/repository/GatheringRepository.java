10a9eb503904e74ed919eb5730a64c550099a403
gamgyul163 <gamgyul163@gmail.com>
2024-05-14 16:41:35 +0900

    🐛 Fix: 모임 리스트 일단 다 불러오게 수정

--- src/main/java/com/withus/withmebe/gathering/repository/GatheringRepository.java
+++ src/main/java/com/withus/withmebe/gathering/repository/GatheringRepository.java
@@ -1,11 +1,13 @@
 package com.withus.withmebe.gathering.repository;
 
 import com.withus.withmebe.gathering.entity.Gathering;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GatheringRepository extends JpaRepository<Gathering, Long> {
 
+    List<Gathering> findAll();
     Optional<Gathering> findById(Long aLong);
 
 }
