a198d26f191cf233c8ddd3db20fb83ccdcd5e8ce
gamgyul163 <gamgyul163@gmail.com>
2024-05-16 01:23:45 +0900

    ✨ Features:Enum타입 소문자 string으로 넣어도 되게 처리

---
+++ src/main/java/com/withus/withmebe/search/type/converter/SearchRangeConverter.java
@@ -0,0 +1,13 @@
+package com.withus.withmebe.search.type.converter;
+
+import com.withus.withmebe.search.type.SearchOption;
+import com.withus.withmebe.search.type.SearchRange;
+import org.springframework.core.convert.converter.Converter;
+
+public class SearchRangeConverter implements Converter<String, SearchRange> {
+
+  @Override
+  public SearchRange convert(String request) {
+    return SearchRange.of(request);
+  }
+}
