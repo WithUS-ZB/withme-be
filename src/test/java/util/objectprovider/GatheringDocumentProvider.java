76184ce9c8c554fb7b4a1933cb8b19ca2784b298
gamgyul163 <gamgyul163@gmail.com>
2024-05-16 01:26:33 +0900

    ♻️ Refactor: GatheringDocument 구조 변화로 사용 불가능하여 일시적으로 주석처리

--- src/test/java/util/objectprovider/GatheringDocumentProvider.java
+++ src/test/java/util/objectprovider/GatheringDocumentProvider.java
@@ -8,25 +8,25 @@ public class GatheringDocumentProvider {
   private GatheringDocumentProvider() {
   }
 
-  public static GatheringDocument getStubbedGatheringDocument(long documentId) {
-    return GatheringDocument.builder()
-        .id(documentId)
-        .nickName("nickName" + documentId)
-        .title("title" + documentId)
-        .gatheringType("gatheringType" + documentId)
-        .maximumParticipant(10 + documentId)
-        .startDttm(LocalDateTime.now())
-        .endDttm(LocalDateTime.now())
-        .applicationDeadLine(LocalDateTime.now())
-        .address("address" + documentId)
-        .detailedAddress("detailedAddress" + documentId)
-        .mainImg("mainImg" + documentId)
-        .participantsType("participantsType" + documentId)
-        .fee(200 + documentId)
-        .participantSelectionMethod("participantSelectionMethod" + documentId)
-        .likeCount(100 + documentId)
-        .createdDttm(LocalDateTime.now())
-        .status("status" + documentId)
-        .build();
-  }
+//  public static GatheringDocument getStubbedGatheringDocument(long documentId) {
+//    return GatheringDocument.builder()
+//        .id(documentId)
+//        .nickName("nickName" + documentId)
+//        .title("title" + documentId)
+//        .gatheringType("gatheringType" + documentId)
+//        .maximumParticipant(10 + documentId)
+//        .startDttm(LocalDateTime.now())
+//        .endDttm(LocalDateTime.now())
+//        .applicationDeadLine(LocalDateTime.now())
+//        .address("address" + documentId)
+//        .detailedAddress("detailedAddress" + documentId)
+//        .mainImg("mainImg" + documentId)
+//        .participantsType("participantsType" + documentId)
+//        .fee(200 + documentId)
+//        .participantSelectionMethod("participantSelectionMethod" + documentId)
+//        .likeCount(100 + documentId)
+//        .createdDttm(LocalDateTime.now())
+//        .status("status" + documentId)
+//        .build();
+//  }
 }
