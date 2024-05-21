9724ba2e2a1667cd89791b95349c96a9047bdfcd
gamgyul163 <gamgyul163@gmail.com>
2024-05-22 02:41:54 +0900

    ✨ Features: MyParticipationSimpleInfo 모임 정보 추가

--- src/main/java/com/withus/withmebe/participation/dto/MyParticipationSimpleInfo.java
+++ src/main/java/com/withus/withmebe/participation/dto/MyParticipationSimpleInfo.java
@@ -1,14 +1,22 @@
 package com.withus.withmebe.participation.dto;
 
-import com.withus.withmebe.participation.type.Status;
+import com.withus.withmebe.gathering.Type.GatheringType;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import lombok.Builder;
 
 @Builder
 public record MyParticipationSimpleInfo(
     Long id,
+    Long gatheringId,
     String title,
-    Status status,
+    String mainImg,
+    LocalDate day,
+    LocalTime time,
+    GatheringType gatheringType,
+    com.withus.withmebe.gathering.Type.Status gatheringStatus,
+    com.withus.withmebe.participation.type.Status status,
     LocalDateTime updatedDttm
 
 ) {
