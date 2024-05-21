d52fa06feb6ff53e791d2955da8d70bc1c9c13c2
je-pa <jieun0502@gmail.com>
2024-05-21 23:12:09 +0900

    ♻️ Refactor code: 인증 확인 로직 삭제

--- src/main/java/com/withus/withmebe/chat/controller/ChatRoomController.java
+++ src/main/java/com/withus/withmebe/chat/controller/ChatRoomController.java
@@ -2,10 +2,7 @@ package com.withus.withmebe.chat.controller;
 
 import com.withus.withmebe.chat.dto.response.ChatRoomDto;
 import com.withus.withmebe.chat.service.ChatRoomService;
-import com.withus.withmebe.common.exception.CustomException;
-import com.withus.withmebe.common.exception.ExceptionCode;
 import com.withus.withmebe.security.anotation.CurrentMemberId;
-import com.withus.withmebe.security.anotation.CurrentUserIsMobileAuthenticatedMember;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,12 +20,8 @@ public class ChatRoomController {
   @PostMapping
   public ResponseEntity<ChatRoomDto> add(
       @CurrentMemberId Long currentMemberId,
-      @CurrentUserIsMobileAuthenticatedMember boolean isMobileAuthenticatedMember,
       @RequestParam Long gatheringId
   ) {
-    if (!isMobileAuthenticatedMember) {
-      throw new CustomException(ExceptionCode.AUTHENTICATION_ISSUE);
-    }
     return ResponseEntity.ok(
         chatRoomService.create(currentMemberId, gatheringId)
     );
