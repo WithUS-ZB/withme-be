3657f638a8615171b1d0cb9771638850f0f0fc9a
je-pa <jieun0502@gmail.com>
2024-05-22 21:41:36 +0900

    ✅ Test: [chatroom] ChatRoomService 테스트 코드 작성

---
+++ src/test/java/util/objectprovider/ChatRoomProvider.java
@@ -0,0 +1,16 @@
+package util.objectprovider;
+
+import com.withus.withmebe.chat.entity.ChatRoom;
+import com.withus.withmebe.gathering.entity.Gathering;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class ChatRoomProvider {
+  private ChatRoomProvider(){}
+
+  public static ChatRoom getStubbedChatRoom(Long chatRoomId, Gathering gathering){
+    ChatRoom chatRoom = ChatRoom.builder()
+        .gathering(gathering).build();
+    ReflectionTestUtils.setField(chatRoom, "id", chatRoomId);
+    return chatRoom;
+  }
+}
