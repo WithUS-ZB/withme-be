4bd372cf2aff71f2ddaa6012fb14301c48c5cbc0
gamgyul163 <gamgyul163@gmail.com>
2024-05-24 17:19:44 +0900

    ♻️ Refactor: sse 연결 요청 엔드포인트 변경  - 멤버id 토큰이 아닌 쿼리스트링으로 전달받도록 변경

--- src/main/java/com/withus/withmebe/security/config/SecurityConfig.java
+++ src/main/java/com/withus/withmebe/security/config/SecurityConfig.java
@@ -70,6 +70,7 @@ public class SecurityConfig {
                     , "/api/search/**"
                     , "/api/participation/count"
                     , "/api/festival"
+                    , "/api/notification/subscribe"
                 ).permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/gathering/**")
                 .permitAll()
