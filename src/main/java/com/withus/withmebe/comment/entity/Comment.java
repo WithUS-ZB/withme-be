2b4a88b417d908486b6146ed6475cbb748430c01
gamgyul163 <gamgyul163@gmail.com>
2024-05-25 17:37:39 +0900

    ♻️ Refactor: 미사용 import 제거

--- src/main/java/com/withus/withmebe/comment/entity/Comment.java
+++ src/main/java/com/withus/withmebe/comment/entity/Comment.java
@@ -15,7 +15,6 @@ import jakarta.persistence.ManyToOne;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
