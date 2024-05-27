bc3d6141db8024417ca7a7f0f43bd4d62c593c4c
gamgyul163 <gamgyul163@gmail.com>
2024-05-27 17:49:54 +0900

    ♻️ Refactor: ordrId, ordrMony ordrChk에서 파싱하도록 변경

--- src/main/java/com/withus/withmebe/payment/dto/request/ApprovePaymentRequest.java
+++ src/main/java/com/withus/withmebe/payment/dto/request/ApprovePaymentRequest.java
@@ -1,11 +1,15 @@
 package com.withus.withmebe.payment.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApprovePaymentRequest{
     @NotBlank
     private String encData;
